94d7e547c59f93cbad1dabf79484753e67463d50
JasonWu <1609adm@gmail.com>
2022-09-07 01:09:36 -0500

    refact(CSS):tuned series of CSS parameters and changed the theme of the index page

--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import ForumPage from './view/ForumPage/ForumPage';
-import 'antd/dist/antd.min.css';
-const root = ReactDOM.createRoot(document.getElementById('root'))
-root.render(<ForumPage/>)
-
+import React from "react";
+import ReactDOM from "react-dom/client";
+import ForumPage from "./view/ForumPage/ForumPage";
+import "antd/dist/antd.min.css";
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(<ForumPage />);
